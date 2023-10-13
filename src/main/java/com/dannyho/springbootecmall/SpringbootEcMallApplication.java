41f4af7ed340eb1f082f205b299d2998bc6e4e7a
DannyHo <zxcv86074@gmail.com>
2023-10-13 14:33:49 +0800

    init spring boot project

---
+++ src/main/java/com/dannyho/springbootecmall/SpringbootEcMallApplication.java
@@ -0,0 +1,13 @@
+package com.dannyho.springbootecmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dannyho/springbootecmall/SpringbootEcMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyho.springbootecmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
